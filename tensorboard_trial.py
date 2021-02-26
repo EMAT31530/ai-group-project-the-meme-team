c3925fdffba4215d1ee0206b38445e6e1b1f6b9d
smruthi-radhakrishnan <62658536+smruthi-radhakrishnan@users.noreply.github.com>
2021-02-26 10:58:04 +0000

    script successfully parses log to dicts and exports to wandb

--- tensorboard_trial.py
+++ tensorboard_trial.py
@@ -12,92 +12,99 @@ import matplotlib.pyplot as plt
 np.set_printoptions(threshold=sys.maxsize)
 
 def main():
-        #path_to_events_file = sys.argv[1]
+	path_to_events_file = sys.argv[1]
 
-        # Temporary hardcoded path for ease of debug
-        path_to_events_file = r"Agent_Training\results\2021.02.10_1700_RocketLander_SR\RocketLander\events.out.tfevents.1612976372.DESKTOP-6RE48I1.13812.0"
+    # Temporary hardcoded path for ease of debug
+    #path_to_events_file = "Agent_Training\results\2021.02.10_1700_RocketLander_SR\RocketLander\events.out.tfevents.1612976372.DESKTOP-6RE48I1.13812.0"
 
-        learning_rate = 0
-        param_dict = {"steps": 1e8,
-                                   "batch_size": 1024,
-                                   "gamma": 0.99,
-                                   "lambda": 0.95
-                                   }
+    # learning_rate = 0
+	param_dict = {"steps": 1e8,
+	                           "batch_size": 1024,
+	                           "gamma": 0.99,
+	                           "lambda": 0.95
+	                           }
 
-        tf_size_guidance = {
-            'compressedHistograms': 10,
-            'images': 0,
-            'scalars': 1e8,
-            'histograms': 1
-        }
+	tf_size_guidance = {
+	    'compressedHistograms': 10,
+	    'images': 0,
+	    'scalars': 1e8,
+	    'histograms': 1
+	}
         
-        event_acc = EventAccumulator(path_to_events_file, tf_size_guidance)
-        event_acc.Reload()
+	event_acc = EventAccumulator(path_to_events_file, tf_size_guidance)
+	event_acc.Reload()
 
-        rows = len(event_acc.Scalars('Is Training')) + 1
-        columns = len(event_acc.Tags()['scalars']) + 1
+	rows = len(event_acc.Scalars('Is Training')) + 1
+	columns = len(event_acc.Tags()['scalars']) + 1
 
+	# # Create empty dictionary for scalar step and value data
+	steps = {}
+	values = {}
+	#Fetch list of scalars
+	scalars = list(event_acc.Tags()['scalars'])
 
+	# Iterate through scalars, and add the relevant components to the dictionary
+	for scalar in scalars:
+		steps[scalar] = [item.step for item in event_acc.Scalars(scalar)]
+		values[scalar] = [item.value for item in event_acc.Scalars(scalar)]
+		unique_steps = sorted(set(step for step in steps[scalar]))
 
+	wandb.init(project="test-drive-2", config=param_dict)
 
-        # Create empty dictionary for scalar step and value data
-        steps = {}
-        values = {}
+	for step in unique_steps:
+		for scalar in scalars:
+			if step in steps[scalar]:
+				step_index = steps[scalar].index(step)
+				print("Step: ", step, "Scalar: ", scalar, "Step position in dict: ", step_index, "Corresponding value: ", values[scalar][step_index])
+				wandb.log({scalar: values[scalar][step_index]}, step=step)
 
-        # Fetch list of scalars
-        scalars = list(event_acc.Tags()['scalars'])
+	wandb.finish()
+	quit()
 
-        # Iterate through scalars, and add the relevant components to the dictionary
-        for scalar in scalars:
-                steps[scalar] = [item.step for item in event_acc.Scalars(scalar)]
-                values[scalar] = [item.value for item in event_acc.Scalars(scalar)]
+        # # Select the data pertaining to one scalar
+        # #s = steps['Policy/Entropy']
+        # #v = values['Policy/Entropy']
+        # s = steps['Policy/Learning Rate']
+        # v = values['Policy/Learning Rate']
 
-        # Select the data pertaining to one scalar
-        #s = steps['Policy/Entropy']
-        #v = values['Policy/Entropy']
-        s = steps['Policy/Learning Rate']
-        v = values['Policy/Learning Rate']
-
-        # Plot and show
-        plt.plot(s, v)
-        plt.show()
-
-
+        # # Plot and show
+        # plt.plot(s, v)
+        # plt.show()
 
         
         #Create empty 2-D array for parsing log data into
-        int_data = np.empty((rows,columns),int)
+        # int_data = np.empty((rows,columns),int)
         #Empty array of object datatype to add category strings
-        cat_data= np.empty((rows,columns),object)
+        # cat_data= np.empty((rows,columns),object)
 
-        categories_list = event_acc.Tags()['scalars']
+        # categories_list = event_acc.Tags()['scalars']
     
-        for cat_index,category in enumerate(categories_list):
-                print(cat_index, category)
-                cat_data[0,cat_index+1]=category
-                for value_index, s in enumerate(event_acc.Scalars(category)):
-                        value_temp = s.value
-                        step_temp = s.step
-                        int_data[value_index+1,0]= step_temp
-                        int_data[value_index+1,cat_index+1]=value_temp
+        # for cat_index,category in enumerate(categories_list):
+        #         print(cat_index, category)
+        #         cat_data[0,cat_index+1]=category
+        #         for value_index, s in enumerate(event_acc.Scalars(category)):
+        #                 value_temp = s.value
+        #                 step_temp = s.step
+        #                 int_data[value_index+1,0]= step_temp
+        #                 int_data[value_index+1,cat_index+1]=value_temp
 
-        # Sort array by ascending order of step to avoid wandb error
-        int_data = int_data[np.argsort(int_data[:, 0])]
-        cat_data[1:,:]=int_data[1:,:]
+        # # Sort array by ascending order of step to avoid wandb error
+        # int_data = int_data[np.argsort(int_data[:, 0])]
+        # cat_data[1:,:]=int_data[1:,:]
         
-        wandb.init(project="test-drive-2", config=param_dict)
-        for row in range(1,rows):
-                for column in range(1,columns):
-                        category = cat_data[0,column]
-                        if(cat_data[row,column]==None):
-                                log_value=None
-                        else:
-                                log_value=float(cat_data[row,column])
-                        log_step=cat_data[row,0]
-                        wandb.log({category: log_value}, step=log_step)
+        # wandb.init(project="test-drive-2", config=param_dict)
+        # for row in range(1,rows):
+        #         for column in range(1,columns):
+        #                 category = cat_data[0,column]
+        #                 if(cat_data[row,column]==None):
+        #                         log_value=None
+        #                 else:
+        #                         log_value=float(cat_data[row,column])
+        #                 log_step=cat_data[row,0]
+        #                 wandb.log({category: log_value}, step=log_step)
 
-        wandb.finish()
-        quit()
+        # wandb.finish()
+        # quit()
 
 if __name__=="__main__":
-        main()
+    main()
